4e952d287359fc1edd7ba95e1982fb7f5926c141
washingtonsilva <washington.silva@ifmg.edu.br>
2017-09-04 21:04:13 -0300

    Cleaning data

--- scripts/clean.R
+++ scripts/clean.R
@@ -15,25 +15,75 @@ str(dados)
 dados$datetime <- as.POSIXct(dados$datetime, format="%m-%d-%YT%H:%M",  tz = "America/Sao_Paulo")
 str(dados$datetime)
 head(dados)
+tail(dados)
 
 #### Verifando a data.frame
 vis_dat(dados)
 vis_miss(dados)
 
-#### Outros tipos de data.frame
-
-# Sem coluna de data-tempo
-dados_stempo <- dados[,-1]
-str(dados_stempo)
-
-# Data.frame long
-dados_long <- stack(dados_stempo)
-head(dados_long)
-colnames(dados_long) <- c("Temperatura", "Sensores")
-str(dados_long)
-
-
 #### Salvando como .Rdata
 save(dados, file = "dados.Rdata")
-save(dados_stempo, file = "dados_stempo.Rdata")
-save(dados_long, file = "dados_long.Rdata")
+
+###############################################################
+###############       SUBSETTING          #####################  
+###############################################################
+
+############################################################
+## df1 <- Antes de 2017-08-22 17:00
+df1 <- subset(dados, datetime < as.POSIXct('2017-08-22 17:00'))
+head(df1)
+tail(df1)
+save(df1, file = "df1.Rdata")
+
+# df2 = df1 sem a coluna 1 (data-tempo) - wide format
+df2 <- df1[,-1]
+head(df2)
+tail(df2)
+save(df2, file = "df2.Rdata")
+
+# df3 = df2 long format
+df3 <- stack(df2)
+head(df3)
+colnames(df3) <- c("Temperatura", "Sensores")
+tail(df3)
+save(df3, file = "df3.Rdata")
+
+############################################################
+## df4 <- Após 2017-08-22 17:00 e antes de 2017-08-22 18:25
+df4 <- subset(dados, datetime >= as.POSIXct('2017-08-22 17:00') 
+              & datetime < as.POSIXct('2017-08-22 18:25'))
+head(df4)
+tail(df4)
+save(df4, file = "df4.Rdata")
+
+# df5 = df4 sem a coluna 1 (data-tempo) - wide format
+df5 <- df4[,-1]
+tail(df5)
+save(df5, file = "df5.Rdata")
+
+# df6 = df5 long format
+df6 <- stack(df5)
+head(df6)
+colnames(df6) <- c("Temperatura", "Sensores")
+tail(df6)
+save(df6, file = "df6.Rdata")
+
+############################################################
+## df7 <- Após 2017-08-22 18:25
+df7 <- subset(dados, datetime > as.POSIXct('2017-08-22 18:25'))
+head(df7)
+tail(df7)
+save(df7, file = "df7.Rdata")
+
+# df8 = df7 sem a coluna 1 (data-tempo) - wide format
+df8 <- df7[,-1]
+head(df8)
+tail(df8)
+save(df8, file = "df8.Rdata")
+
+# df9 = df8 long format
+df9 <- stack(df8)
+head(df9)
+colnames(df9) <- c("Temperatura", "Sensores")
+tail(df9)
+save(df9, file = "df9.Rdata")
